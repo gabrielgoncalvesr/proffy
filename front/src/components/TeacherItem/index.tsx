90c4bc29c093c9cb1f4a56a4d7c867e8fc41956a
Gabriel Gonçalves Reis <greis@spo.office.br.lyra>
2020-08-09 03:23:36 -0300

    adding api connection

--- front/src/components/TeacherItem/index.tsx
+++ front/src/components/TeacherItem/index.tsx
@@ -3,34 +3,52 @@ import React from 'react';
 import whatsAppIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-const TeacherItem = () => {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+    const createNewConnection = () => {
+        api.post('/connections', {
+            user_id: teacher.id
+        })
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://cdn.iconscout.com/icon/free/png-512/avatar-372-456324.png" alt="Avatar" />
+                <img src={teacher.avatar} alt="Avatar" />
                 <div>
-                    <strong>Diego Fernandes</strong>
-                    <span>Química</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-                Entusiasta das melhores tecnologias de química avançada.
-                    <br /><br />
-                Apaixonado por explodir coisas em laboratórios e por mudar a vida das pessoas através de experiências.
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/Hora
-                    <strong>R$ 80,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
 
-                <button type="button">
+                <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`} type="button">
                     <img src={whatsAppIcon} alt="WhatsApp" />
                         Entrar em Contato
-                </button>
+                </a>
             </footer>
         </article>
     );
